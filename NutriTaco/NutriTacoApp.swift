e34e3299432f44ac32c024874f99fb625168738d
Victor Brigido <victorbrigido@icloud.com>
2024-02-05 10:39:51 -0300

    Initial Commit

---
+++ NutriTaco/NutriTacoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriTacoApp.swift
+//  NutriTaco
+//
+//  Created by Victor Brigido on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct NutriTacoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
